e6218abb2f0a520a9cd33d823a6ba40e09f6ed3c
Li-Yi Wei <git@liyiwei.org>
2026-08-01 16:06:50 -0700

    add gemini option

--- python/m1/m1.2_scratch_agent.py
+++ python/m1/m1.2_scratch_agent.py
@@ -1,9 +1,21 @@
 from deepagents import create_deep_agent
 
 from models import model
+from langchain.chat_models import init_chat_model
+
+use_gemini = True
+
+if use_gemini:
+    #model = "google_genai:gemini-2.5-flash"
+    model = init_chat_model("google_genai:gemini-2.5-flash")
 
 agent = create_deep_agent(model=model)
 
 result = agent.invoke({"messages": [{"role": "user", "content": "What is an LLM?"}]})
 
-print(result["messages"][-1].content)
+if use_gemini:
+    answer = result["messages"][-1].content[0]["text"]
+else:
+    answer = result["messages"][-1].content
+
+print(answer)
