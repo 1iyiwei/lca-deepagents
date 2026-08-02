46421aa15a5c702c9dbb14c0de3af564ac986109
Li-Yi Wei <git@liyiwei.org>
2026-08-01 18:06:45 -0700

    routine to print content across different models; tested with only anthropic and gemini for now

--- python/m1/m1.2_scratch_agent.py
+++ python/m1/m1.2_scratch_agent.py
@@ -1,21 +1,11 @@
 from deepagents import create_deep_agent
 
 from models import model
+from my_utils import print_content
 from langchain.chat_models import init_chat_model
-
-use_gemini = True
-
-if use_gemini:
-    #model = "google_genai:gemini-2.5-flash"
-    model = init_chat_model("google_genai:gemini-2.5-flash")
 
 agent = create_deep_agent(model=model)
 
 result = agent.invoke({"messages": [{"role": "user", "content": "What is an LLM?"}]})
 
-if use_gemini:
-    answer = result["messages"][-1].content[0]["text"]
-else:
-    answer = result["messages"][-1].content
-
-print(answer)
+print_content(model, result["messages"][-1])
---
+++ python/my_utils.py
@@ -0,0 +1,14 @@
+# my own utils
+
+# get the provider (e.g. "anthropic", "google_genai", "openai") of a model
+# created via init_chat_model(), regardless of the model string used to init it
+def get_provider(model):
+    return model._get_ls_params()["ls_provider"]
+
+
+# print the content of a message only without other extra meta data based on the model's provider
+def print_content(model, message):
+    if get_provider(model) == "google_genai":
+        print(message.content[0]["text"])
+    else:
+        print(message.content)
